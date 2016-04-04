58728fc310c07c73b3f5ae5b494d4f29080da9c2
Hauke Stieler <mail@hauke-stieler.de>
2016-04-05 00:08:58 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-go build -o bin/g0Ch@_client g0Ch@_client/*.go
-go build -o bin/g0Ch@_server g0Ch@_server/*.go
+go build -o bin/g0Ch@_client src/g0Ch@_client/*.go
+go build -o bin/g0Ch@_server src/g0Ch@_server/*.go
